782f38304f510117d03b9970141bf4b44794cb13
KUNF24 <kunf5616@gmail.com>
2024-12-03 17:03:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ logo = """
          ÷÷    ÷÷    ÷÷             ÷÷               ÷÷
          ÷÷      ÷÷  ÷÷            ÷÷ ÷÷ ÷÷          ÷÷
 _—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_             
-_—\033[32m              THIS SCRIPT IS A DEDICATION                 _—
-_—\033[33m         TO THE STABILIZATION OF THE MARTYRS              _—
-_—\033[34m                     design by: KunF24                    _—
-_—\033[35m                         ——oO0Oo——                        _—
+_—\033[32m              THIS SCRIPT IS A DEDICATION                   _—
+_—\033[33m         TO THE STABILIZATION OF THE MARTYRS                _—
+_—\033[34m                     design by: KunF24                      _—
+_—\033[35m                         ——oO0Oo——                          _—
 _—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_
 """
 faded_text = fade.fire(logo)
