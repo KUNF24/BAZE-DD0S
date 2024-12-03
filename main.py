e5d65fa74d817e1fc084dff3eec929e46c287086
KUNF24 <kunf5616@gmail.com>
2024-12-03 15:21:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ async def increment_view_count(session):
         async with session.get(url) as response:
             if response.status == 200:
                 print("[+]  \033[32mRequest Sent  \033[33m" +url+"  \033[34mattack..!!  \033[0m")
-                print("[+]  \033[95mRequest
-    else:
+        else:
                 print("[!]  \033[96mRequest Sent  \033[32m" +url+"  \033[33mattack..!!  \033[0m")
                 print("[!]  \033[94mRequest Sent  \033[96m" +url+"  \033[37mattack..!!  \033[0m")
     except aiohttp.ClientError as e:
